56211a82c1cc285748235768278bc11002257810
DanielBuselli <buselli.daniel@gmail.com>
2020-04-28 14:13:33 +0300

    CORS update

--- src/main/java/ohpro1/formsite/web/AnswerController.java
+++ src/main/java/ohpro1/formsite/web/AnswerController.java
@@ -1,6 +1,8 @@
 package ohpro1.formsite.web;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ohpro1.formsite.domain.Answer;
 import ohpro1.formsite.domain.AnswerRepository;
 
-
+@CrossOrigin(maxAge = 3600)
 @RestController
 public class AnswerController {
 	private final AnswerRepository arepo;
