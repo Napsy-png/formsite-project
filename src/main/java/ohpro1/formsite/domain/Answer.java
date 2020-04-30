fdafcccc677c6d84ed20d1b9e0800ed79e5e74d0
DanielBuselli <buselli.daniel@gmail.com>
2020-04-30 19:54:16 +0300

    undo

--- src/main/java/ohpro1/formsite/domain/Answer.java
+++ src/main/java/ohpro1/formsite/domain/Answer.java
@@ -11,78 +11,32 @@ import lombok.Data;
 @Entity
 public class Answer {
 	private @Id @GeneratedValue(strategy=GenerationType.AUTO) Long id;
-	private String answer1, answer2, answer3, answer4, answer5, answer6, answer7;
+	private String answers;
 	
 	public Answer() {
 		super();
 	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getAnswer1() {
-		return answer1;
-	}
-
-	public void setAnswer1(String answer1) {
-		this.answer1 = answer1;
-	}
-
-	public String getAnswer2() {
-		return answer2;
-	}
-
-	public void setAnswer2(String answer2) {
-		this.answer2 = answer2;
-	}
-
-	public String getAnswer3() {
-		return answer3;
-	}
-
-	public void setAnswer3(String answer3) {
-		this.answer3 = answer3;
-	}
-
-	public String getAnswer4() {
-		return answer4;
-	}
-
-	public void setAnswer4(String answer4) {
-		this.answer4 = answer4;
-	}
-
-	public String getAnswer5() {
-		return answer5;
-	}
-
-	public void setAnswer5(String answer5) {
-		this.answer5 = answer5;
-	}
-
-	public String getAnswer6() {
-		return answer6;
-	}
-
-	public void setAnswer6(String answer6) {
-		this.answer6 = answer6;
-	}
-
-	public String getAnswer7() {
-		return answer7;
-	}
-
-	public void setAnswer7(String answer7) {
-		this.answer7 = answer7;
-	}
 	
-		
-		
+		public Answer(String answers){
+			super();
+			this.answers = answers;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getAnswerst() {
+			return answers;
+		}
+
+		public void setAnswers(String answers) {
+			this.answers = answers;
+		}
 	
 
 }
