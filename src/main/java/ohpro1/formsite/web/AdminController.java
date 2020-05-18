1a99a70d5459247d05b5129bcbbb287362d9253c
DanielBuselli <buselli.daniel@gmail.com>
2020-05-18 23:43:23 +0300

    bug fix

--- src/main/java/ohpro1/formsite/web/AdminController.java
+++ src/main/java/ohpro1/formsite/web/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import ohpro1.formsite.domain.Question;
 import ohpro1.formsite.domain.QuestionRepository;
 
+@CrossOrigin(maxAge = 3600)
 @Controller
 public class AdminController {
 	@Autowired
