fd06e536386879463af86986a3d4a52334124afb
DanielBuselli <buselli.daniel@gmail.com>
2020-05-19 00:57:07 +0300

    yet another fix

--- src/main/java/ohpro1/formsite/WebConfig.java
+++ src/main/java/ohpro1/formsite/WebConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @EnableWebMvc
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig extends WebMvcConfigurerAdapter {
 	
 	public void addCorsMapping(CorsRegistry registry) {
 		registry.addMapping("/**")
